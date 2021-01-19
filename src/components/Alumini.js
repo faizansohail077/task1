3f9fee3e9410b004e9f7228bf1b74d5aff5eddbe
Muhammad Faizan <faizansohail077@gmail.com>
2021-01-19 01:16:13 -0500

    about page completed

--- src/components/Alumini.js
+++ src/components/Alumini.js
@@ -1,9 +1,31 @@
 import React from 'react'
 import './Alumini.css'
+import img from '../img/aluminiPic.png'
 const Alumini = () => {
     return (
         <div>
-            
+            <div className="alumini">
+                <img className="alumini__image" src={img} alt='' />
+
+                <p className="alumini__text">
+                    <span>Lorem Ipium</span><br />
+                    <div className="alumini__text1">
+                        Lorem Ipsum is simply dummy text of the printing  </div>
+                    <br />
+                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book
+                </p>
+
+            </div>
+            <div className="alumini2">
+                <p className="alumini__text">
+                    <span>Lorem Ipium</span><br />
+                    <div className="alumini__text1">
+                        Lorem Ipsum is simply dummy text of the printing  </div>
+                    <br />
+                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book
+                </p>
+                <img className="alumini__image" src={img} alt='' />
+            </div>
         </div>
     )
 }
--- src/screens/Aboutus.js
+++ src/screens/Aboutus.js
@@ -1,12 +1,22 @@
 import React from 'react'
+import Alumini from '../components/Alumini'
 import Icons from '../components/Icons'
 import Navbar2 from '../components/Navbar2'
+import Logos from '../components/logos'
+import Testimonials from '../components/Testimonials'
+import Work from '../components/Work'
+
+
 
 const Aboutus = () => {
     return (
         <div>
-            <Navbar2/>
-            <Icons/>
+            <Navbar2 />
+            <Icons />
+            <Alumini />
+            <Logos />
+            <Work />
+            <Testimonials />
         </div>
     )
 }
